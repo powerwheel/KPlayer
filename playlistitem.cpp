bd77589c7a3a1871fe96cfcdcbe2b094935f8340
powerwheel <november123@163.com>
2014-04-22 14:24:20 +0800

    modified:   playlistitem.cpp 	modified:   playlistitem.h

--- playlistitem.cpp
+++ playlistitem.cpp
@@ -1,5 +1,99 @@
 #include "playlistitem.h"
 
-PlayListItem::PlayListItem()
+PlayListItem::PlayListItem():QMap<KPlayer::MetaData,QString>(),
+    m_flag(FREE)
 {
+    m_info = 0;
+    m_length = 0;
+    m_selected = false;
+    m_current = false;
+}
+
+PlayListItem::PlayListItem(const PlayListItem &item):
+    QMap<KPlayer::MetaData,QString>(item),
+    m_flag(item.m_flag)
+{
+    m_title = item.m_title;
+    if(item.m_info){
+        m_info = new FileInfo(*(item.m_info));
+    }else{
+        m_info = 0;
+    }
+    m_selected = item.m_selected;
+    m_current = item.m_current;
+    m_length = item.m_length;
+}
+PlayListItem::PlayListItem(FileInfo *info):
+    QMap<KPlayer::MetaData,QString>(info->metaData()),
+    m_flag(FREE)
+{
+    m_length = info->length();
+    m_selected = false;
+    m_current = false;
+    m_info = info;
+    insert(KPlayer::URL,m_info->path());
+}
+PlayListItem::~PlayListItem(){
+    if(m_info)
+        delete m_info;
+}
+void PlayListItem::setSelected(bool select)
+{
+    m_selected = select;
+}
+bool PlayListItem::isSelected()
+{
+    return m_selected;
+}
+void PlayListItem::setCurrent(bool yes)
+{
+    m_current = yes;
+}
+bool PlayListItem::isCurrent()
+{
+    return m_current;
+}
+FLAGS PlayListItem::flags() const
+{
+    return m_flag;
+}
+void PlayListItem::setFlags(FLAGS f)
+{
+    m_flag = f;
+}
+const PlayListItem::text()
+{
+    if(m_title.isEmpty())
+        readMetaData();
+    return m_title;
+}
+void PlayListItem::setText(const QString &title)
+{
+    m_title = title;
+}
+qint64 PlayListItem::length() const
+{
+    return m_length;
+}
+void PlayListItem::setLength(qint64 length)
+{
+    m_length = length;
+}
+
+const PlayListItem::url() const
+{
+    return value(KPlayer::URL);
+}
+void PlayListItem::updateMetaData(const QMap<KPlayer::MetaData, QString> &metaData)
+{
+    QMap<KPlayer::MetaData,QString>::operator =(metaData);
+    readMetaData();
+}
+void PlayListItem::updateTags()
+{
+
+}
+void PlayListItem::readMetaData()
+{
+
 }
--- playlistitem.h
+++ playlistitem.h
@@ -6,6 +6,13 @@
 class PlayListItem: public QMap<KPlayer::MetaData,QString>
 {
 public:
+
+    enum FLAGS{
+        FREE = 0;
+        EDITING,
+        SCHEDULED_FOR_DELETION
+    };
+
     PlayListItem();
 
     PlayListItem(const PlayListItem &item);
@@ -13,8 +20,38 @@ public:
     PlayListItem(FileInfo *info);
     ~PlayListItem();
 
+    void setSelected(bool select);
+    bool isSelected() const;
+
+    void setCurrent(bool yes);
+    void isCurrent() const;
+
+    FLAGS flags() const;
+    void setFlags(FLAGS);
+
+    const QString text();
+    void setText(const QString& title);
+
+    qint64 length() const;
+
+    void setLength(qint64 length);
 
 
+    const QString url() const;
+
+    void updateMetaData(const QMap<KPlayer::MetaData,QString> &metaData);
+
+    void updateTags();
+
+
+private:
+    void readMetaData();
+    QString m_title;
+    FileInfo *m_info;
+    bool m_selected;
+    bool m_current;
+    FLAGS m_flag;
+    qint64 m_length;
 };
 
 #endif // PLAYLISTITEM_H
